849c4c38faa088cca823450176f54bb6aacaa5b3
Garve Hays <gkhays@gmail.com>
2023-11-17 16:14:38 -0600

    Add price info and output as table

--- top_games.py
+++ top_games.py
@@ -1,8 +1,9 @@
 import requests
 import yaml
+from tabulate import tabulate
 
 # Read API and Steam keys from config.yaml
-def read_keys_from_yaml(file_path):
+def read_configuration(file_path):
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
         game_count = config.get('game_count', 10) # Default to 10 games
@@ -24,34 +25,53 @@ def get_top_games(api_key, steam_id, game_count):
         return None
 
 # Get game name by appid
-def get_game_name(app_id):
+def get_game_details(app_id):
     url = f'https://store.steampowered.com/api/appdetails?appids={app_id}'
     response = requests.get(url)
     
     if response.status_code == 200:
         data = response.json()
         if data[str(app_id)]['success']:
-            game_name = data[str(app_id)]['data']['name']
-            return game_name
-    return 'Unknown Game'
+            game_data = data[str(app_id)]['data']
+            game_name = game_data['name']
+            price_overview = game_data.get('price_overview', {})
+            price = price_overview.get('final_formatted', 'Free to Play')
+            return game_name, price
+    return 'Unknown Game', 0, 'Free to Play'
 
 # Convert minutes to hours
 def convert_to_hours(minutes):
     return minutes / 60
 
-# Display top ten games based on playtime
+# Display top games based on playtime
 def display_top_games(top_games, game_count):
     if top_games:
         print(f"Top {game_count} Games Based on Hours Played:")
         for idx, game in enumerate(top_games, start=1):
             app_id = game['appid']
-            game_name = get_game_name(app_id)
+            game_name, price = get_game_details(app_id)
             playtime_hours = convert_to_hours(game['playtime_forever'])
-            print(f"{idx}. {game_name} - {playtime_hours:.2f} hours")
+            print(f"{idx}. {game_name} - {playtime_hours:.2f} hours - Price: {price}")
+    else:
+        print("No data available")
+
+# Display top games in a table
+def display_top_games_table(top_games, game_count):
+    table_data = []
+    if top_games:
+        print(f"Top {game_count} Games Based on Hours Played:")
+        for idx, game in enumerate(top_games, start=1):
+            app_id = game['appid']
+            game_name, price = get_game_details(app_id)
+            playtime_hours = convert_to_hours(game['playtime_forever'])
+            table_data.append([idx, game_name, f"{playtime_hours:.2f} hours", price])
+
+        headers = ["Rank", "Game", "Playtime", "Price"]
+        print(tabulate(table_data, headers=headers, tablefmt="grid"))
     else:
         print("No data available")
 
 if __name__ == "__main__":
-    api_key, steam_id, game_count = read_keys_from_yaml('config.yaml')
+    api_key, steam_id, game_count = read_configuration('config.yaml')
     top_games = get_top_games(api_key, steam_id, game_count)
-    display_top_games(top_games, game_count)
+    display_top_games_table(top_games, game_count)
