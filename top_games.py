a32bbe660c60b58aa40adcb5c58ae8af961454fd
Garve Hays <gkhays@gmail.com>
2023-11-17 15:21:03 -0600

    Add optional config parameter

--- top_games.py
+++ top_games.py
@@ -5,10 +5,11 @@ import yaml
 def read_keys_from_yaml(file_path):
     with open(file_path, 'r') as file:
         config = yaml.safe_load(file)
-    return config.get('api_key'), config.get('steam_id')
+        game_count = config.get('game_count', 10) # Default to 10 games
+    return config.get('api_key'), config.get('steam_id'), game_count
 
 # Get the list of games and playtime for the specified user
-def get_top_games(api_key, steam_id):
+def get_top_games(api_key, steam_id, game_count):
     url = f'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={api_key}&steamid={steam_id}&format=json'
     response = requests.get(url)
     
@@ -16,7 +17,7 @@ def get_top_games(api_key, steam_id):
         data = response.json()
         games = data['response']['games']
         # Sort the games by playtime in descending order
-        sorted_games = sorted(games, key=lambda x: x['playtime_forever'], reverse=True)[:10]
+        sorted_games = sorted(games, key=lambda x: x['playtime_forever'], reverse=True)[:game_count]
         return sorted_games
     else:
         print("Failed to retrieve data")
@@ -39,9 +40,9 @@ def convert_to_hours(minutes):
     return minutes / 60
 
 # Display top ten games based on playtime
-def display_top_games(top_games):
+def display_top_games(top_games, game_count):
     if top_games:
-        print("Top 10 Games Based on Hours Played:")
+        print(f"Top {game_count} Games Based on Hours Played:")
         for idx, game in enumerate(top_games, start=1):
             app_id = game['appid']
             game_name = get_game_name(app_id)
@@ -50,8 +51,7 @@ def display_top_games(top_games):
     else:
         print("No data available")
 
-
 if __name__ == "__main__":
-    api_key, steam_id = read_keys_from_yaml('config.yaml')
-    top_games = get_top_games(api_key, steam_id)
-    display_top_games(top_games)
+    api_key, steam_id, game_count = read_keys_from_yaml('config.yaml')
+    top_games = get_top_games(api_key, steam_id, game_count)
+    display_top_games(top_games, game_count)
